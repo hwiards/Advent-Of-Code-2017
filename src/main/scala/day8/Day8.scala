486df43878576494b7f8dc57b374167af193d75c
Hilko Wiards <hilko.wiards@uni-oldenburg.de>
2017-12-08 16:39:05 +0100

    Day 8 Update

--- src/main/scala/day8/Day8.scala
+++ src/main/scala/day8/Day8.scala
@@ -5,68 +5,74 @@
 import scala.collection.mutable
 import scala.io.Source
 
-object Day8 extends App {
+object Day8 {
+  def main(args: Array[String]): Unit = {
 
-  val lines = Source.fromResource("problems/day8.txt").getLines().toArray
-  var registers: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()
-  var max = 0
+    val lines = Source.fromResource("problems/day8.txt").getLines().toArray
 
-  for (progLine <- lines) {
-    val split = progLine.split("\\s")
-    val register = split(0)
-    val changeValue = split(2).toInt
-    val condValue = split(6).toInt
-    val condition = split(5)
-    val condRegister = split(4)
+    var registers: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()
+    var max = 0
 
-    if (registers.get(register).isEmpty) {
-      registers.put(register, 0)
-    }
-    if (registers.get(condRegister).isEmpty) {
-      registers.put(condRegister, 0)
-    }
+    //Walk throug all lines of the Program
+    for (progLine <- lines) {
+      //Assign the values
+      val split = progLine.split("\\s")
+      val register = split(0)
+      val operation = split(1)
+      val changeValue = split(2).toInt
+      val condValue = split(6).toInt
+      val condition = split(5)
+      val condRegister = split(4)
 
-    if (split(1) == "inc") {
-      IncrementIf(register, changeValue, condRegister, condition, condValue)
-    } else {
-      DecrementIf(register, changeValue, condRegister, condition, condValue)
-    }
+      //If the two registers do not exist yet, create them
+      if (registers.get(register).isEmpty) {
+        registers.put(register, 0)
+      }
+      if (registers.get(condRegister).isEmpty) {
+        registers.put(condRegister, 0)
+      }
 
-    if (registers.get(register).get > max) max = registers.get(register).get
-  }
+      //Match "inc" and "dec"
+      operation match {
+        case "inc" => IncrementIf(register, changeValue, condRegister, condition, condValue)
+        case "dec" => DecrementIf(register, changeValue, condRegister, condition, condValue)
+      }
 
-  println(f"Part1: ${registers.values.max}")
-  println(f"Part2: ${max}")
+      if (registers.get(register).get > max) max = registers.get(register).get
 
-  def IncrementIf(register: String, changeValue: Int, condRegister: String, condition: String, condValue: Int): Unit = {
-
-    if (checkCond(condRegister, condition, condValue)) {
-      val oldValue = registers.get(register).get
-      val newValue = oldValue + changeValue
-      registers.put(register, newValue)
     }
 
-  }
+    println(f"Part1: ${registers.values.max}")
+    println(f"Part2: ${max}")
 
-  def DecrementIf(register: String, changeValue: Int, condRegister: String, condition: String, condValue: Int): Unit = {
-    if (checkCond(condRegister, condition, condValue)) {
-      val oldValue = registers.get(register).get
-      val newValue = oldValue - changeValue
-      registers.put(register, newValue)
-    }
-  }
 
-  def checkCond(condRegister: String, condition: String, condValue: Int): Boolean = {
-
-    condition match {
-      case "==" => return registers.get(condRegister).get == condValue
-      case ">=" => return registers.get(condRegister).get >= condValue
-      case ">" => return registers.get(condRegister).get > condValue
-      case "<=" => return registers.get(condRegister).get <= condValue
-      case "<" => return registers.get(condRegister).get < condValue
-      case "!=" => return registers.get(condRegister).get != condValue
+    //Increment if condition is met.
+    def IncrementIf(register: String, changeValue: Int, condRegister: String, condition: String, condValue: Int): Unit = {
+      if (checkCond(condRegister, condition, condValue)) {
+        registers(register) += changeValue
+      }
     }
 
+    //Decrement if condition is met.
+    def DecrementIf(register: String, changeValue: Int, condRegister: String, condition: String, condValue: Int): Unit = {
+      if (checkCond(condRegister, condition, condValue)) {
+        registers(register) -= changeValue
+      }
+    }
+
+    //Check condition.
+    def checkCond(condRegister: String, condition: String, condValue: Int): Boolean = {
+
+      condition match {
+        case "==" => return registers.get(condRegister).get == condValue
+        case ">=" => return registers.get(condRegister).get >= condValue
+        case ">" => return registers.get(condRegister).get > condValue
+        case "<=" => return registers.get(condRegister).get <= condValue
+        case "<" => return registers.get(condRegister).get < condValue
+        case "!=" => return registers.get(condRegister).get != condValue
+      }
+
+    }
   }
 }
 
